80687880cc8f47464b055a9ef65e49920d40ce36
MoonBasedKas <kassidy.maberry@gmail.com>
2024-11-21 19:49:37 -0700

    Weird bug?

--- website.py
+++ website.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from models import dbCon
 from flask import render_template
 
 app = Flask(__name__)
@@ -7,6 +8,25 @@ app = Flask(__name__)
 @app.route('/<name>')
 def Welcome(name = None):
     return render_template('index.html', person=name)
+
+from flask import Flask, jsonify, request
+ 
+app = Flask(__name__)
+ 
+@app.route('/api/data', methods=['GET', 'POST'])  
+def api_data():
+    if request.method == 'POST':
+        data = request.json  
+        print('Received JSON data:', data)
+        return 'JSON data received successfully!', 200
+    elif request.method == 'GET':
+           db = dbCon.DBCon()
+           z = db.fetchVotesEnc()
+           for i in z:
+                json_data = {}
+                json_data[i.id] = {'Canidate':i.canidate, 'Location':i.location,'Votes':i.count}
+
+    return jsonify(json_data)
 
 @app.route("/hi")
 def hello():
